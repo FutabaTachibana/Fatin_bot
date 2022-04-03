ea58a4b7a1bb93a4b5fd993728250069d7e0444a
FutabaTachibana <60652749+FutabaTachibana@users.noreply.github.com>
2022-04-03 23:36:17 +0800

    Delete Fatin-bot directory

--- Fatin-bot/plugins/debug.py
+++
@@ -1,5 +0,0 @@
-from nonebot import CommandSession, on_command
-
-@on_command('alive', aliases = ('heartbeat', '存活', '存活确认'))
-async def alive(session : CommandSession):
-    await session.send('STATUS OK: ' + session.current_arg.strip())
